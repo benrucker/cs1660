6e2d356d0205de8c851ae40f517a4e7315c267f3
benrucker <brucker117@gmail.com>
2021-11-19 17:42:13 -0500

    Add some sonar starter code

--- Project/frontend/app.py
+++ Project/frontend/app.py
@@ -1,17 +1,53 @@
-SERVICES = ['Hadoop', 'Jupyter Notebook', 'Apache Spark', 'SonarQube & SonarScanner']
+import subprocess
 # look into https://kubernetes.io/docs/tasks/debug-application-cluster/get-shell-running-container/
 
 def launch_service(service):
-    match service:
-        case 'Hadoop':
-            pass
-        case 'Jupyter Notebook':
-            pass
-        case 'Apache Spark':
-            pass
-        case 'SonarQube & SonarScanner':
-            pass
+    SERVICES[service]()
 
+
+def hadoop():
+    pass
+
+
+def jupyter():
+    pass
+
+
+def spark():
+    pass
+
+
+def sonar():
+    print("Would you like to (1) analyze a new project or (2) go straight to SonarQube?")
+    choice = input("> ")
+    if choice == "1":
+        print("Please enter the path to the project you would like to analyze.")
+        path = input("> ")
+        result = subprocess.run(
+            [
+                "kubectl", "exec", "sonar", "--",
+                "curl", "--data-urlencode", f"name={path}&project={path}", "http://localhost:9000/api/projects/create", "-X", "POST"
+            ], check=True, stdout=subprocess.PIPE
+        )
+        result = subprocess.run(
+            [
+                "kubectl", "exec", "sonar", "--",
+                "sonar-scanner", path # add the rest of the arguments, login, project, etc
+            ], check=True, stdout=subprocess.PIPE
+        )
+    #
+    # unconditionally go to sonarqube
+    # get path to sonarqube pod
+    subprocess.run(['xdg-open', "sonar.svc.cluster.local:9000"], check=True)
+    pass
+
+
+SERVICES = {
+    'Hadoop': hadoop,
+    'Jupyter Notebook': jupyter,
+    'Apache Spark': spark,
+    'SonarQube & SonarScanner': sonar,
+}
 
 if __name__ == '__main__':
     print('Welcome to the Microservices Matrix! Which tool would you like to open?')
